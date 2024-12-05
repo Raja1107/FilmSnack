0282fada2abe0a712fcecd3cce0f81ab6001b15a
Raja1107 <rk.acc@tuta.io>
2024-12-05 16:14:27 +0100

    add shadcn

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -5,23 +5,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
+import React from "react";
 import "./tailwind.css";
 import { useEffect } from "react";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 function useDarkMode() {
   useEffect(() => {
