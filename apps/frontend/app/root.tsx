0a72930190a429786ef7502ecb499f5feda16864
Raja1107 <rk.acc@tuta.io>
2024-12-13 20:31:18 +0100

    Add NavigationMenu

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -1,25 +1,24 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Link, Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
-import React from "react";
-import "./tailwind.css";
-import { useEffect } from "react";
+import type React from 'react';
+import './tailwind.css';
+import { useEffect } from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from './components/ui/navigation-menu';
 
 function useDarkMode() {
   useEffect(() => {
-    document.documentElement.classList.add("dark");
+    document.documentElement.classList.add('dark');
   }, []);
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
   useDarkMode();
   return (
-    <html lang="en" className="bg-background">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -27,6 +26,26 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <div className="flex items-center p-4 border-b-2">
+          <nav>
+            <NavigationMenu className="">
+              <NavigationMenuList className="p-4 space-x-6">
+                <NavigationMenuItem className="text-primary text-xl font-bold mr-4">
+                  <Link to="/">FilmSnack</Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="text-muted-foreground hover:text-foreground">
+                  <Link to="/movies">Movies</Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="text-muted-foreground hover:text-foreground">
+                  <Link to="/tv-shows">TV Shows</Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="text-muted-foreground hover:text-foreground">
+                  <Link to="/lists">Lists</Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </nav>
+        </div>
         {children}
         <ScrollRestoration />
         <Scripts />
--- apps/frontend/app/routes/_index.tsx
+++ apps/frontend/app/routes/_index.tsx
@@ -1,21 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "FilmSnack" },
-    { name: "description", content: "Welcome to FilmSnack!" },
-  ];
+  return [{ title: 'FilmSnack' }, { name: 'description', content: 'Welcome to FilmSnack!' }];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center bg-background">
+    <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-primary">
-            Welcome to
-            <span className="sr-only"></span>
-          </h1>
+          <h1 className="leading text-2xl font-bold text-primary">Welcome to</h1>
           <div className="font-bold text-4xl text-primary">FilmSnack</div>
         </header>
       </div>
